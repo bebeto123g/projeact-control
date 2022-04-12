eecc35279b2d24a96a63f83b7bb4529c04e43875
NemtsevHome <bebeto123@mail.ru>
2022-04-12 17:53:03 +0300

    init

---
+++ main.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+echo $(date  +%Y-%m-%d) $(date +%H:%M:%S)
+
+# Переключение на другие ветки
+#cd /home/nemtzev/projects/giper && git pull && git checkout issue-
+#cd /home/nemtzev/projects/domostroyrf && git pull && git checkout issue-
+#cd /home/nemtzev/projects/bank && git pull && git checkout issue-
+#cd /home/nemtzev/projects/vgoroden && git pull && git checkout issue-
+
+# Перезапуск всех контейнеров
+#cd /home/nemtzev/projects/giper && git pull && /usr/local/bin/docker-compose restart
+#cd /home/nemtzev/projects/domostroyrf && git pull && /usr/local/bin/docker-compose restart
+#cd /home/nemtzev/projects/bank && git pull && /usr/local/bin/docker-compose restart
+#cd /home/nemtzev/projects/vgoroden && git pull && /usr/local/bin/docker-compose restart
+
+
+# Пересборка проекта после обновления
+#cd /home/nemtzev/projects/giper && git pull && ./assets-rebuild.sh
+#cd /home/nemtzev/projects/domostroyrf && git pull && ./assets-rebuild.sh
+#cd /home/nemtzev/projects/bank && git pull && git ./assets-rebuild.sh
+#cd /home/nemtzev/projects/vgoroden && git pull && ./assets-rebuild.sh
